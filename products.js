ecb15b9dbf12de7576cc783d175305916a514f18
Masimthembe <mndabeni6@gmail.com>
2021-08-16 15:52:41 +0200

    added to cart

--- products.js
+++ products.js
@@ -29,6 +29,35 @@ function showproducts(products) {
     `;
   });
 }
+
+// Function to render Cart When items are being added//
+function renderCart(cartItems) {
+  cartItems.length > 0;
+  let cartContainer = document.querySelector("#cart");
+  if (cartItems.length > 0) {
+    cartItems.map((cartItem) => {
+      cartContainer.innerHTML += `
+      <div class = "products">
+            <img src="${cartItem.images}" class = "product-image">
+            <div class = "product-content"> 
+                <h4 class = "product-title"> ${cartItem.product_name}</h4>
+                <p class = "product-description"> ${cartItem.description}</p>
+                <p class = "product-price">R${cartItem.price} </p>
+                <button class ="revome_cart" onclick="removeItem()">Remove item</button>
+            </div>
+            
+        </div>
+      
+      
+      `;
+    });
+    let totalPrice = cartItems.reduce((total, item) => total + item.price, 0);
+    cartContainer.innerHTML += `<h3>Total is: ${totalPrice} </h3>`;
+  } else {
+    cartContainer.innerHTML = "<h2> No items in cart</h2>";
+  }
+}
+
 //* Add to cart functions*//
 function addToCart(id) {
   let product = products.data.find((item) => {
@@ -36,6 +65,7 @@ function addToCart(id) {
   });
   console.log(product);
   cart.push(product);
+  renderCart(cart);
   console.log("See Cart Items Here: ", cart);
 }
 
@@ -57,6 +87,8 @@ function searchForProducts() {
   }
 }
 
+// Sorting Item Asc //
+
 function sortNameAsc() {
   let sortedProducts = products.sort((a, b) => {
     if (a.product_name > b.id) return 1;
@@ -66,6 +98,7 @@ function sortNameAsc() {
   showproducts(sortedProducts);
 }
 
+// Sorting Items  Descending //
 function sortNameDesc() {
   let sortedProducts = products.sort((a, b) => {
     if (a.product_name > b.id) return 1;
@@ -74,4 +107,56 @@ function sortNameDesc() {
   });
   sortedProducts.reverse();
   showproducts(sortedProducts);
+}
+
+// Function to toggleCart //
+
+function toggleCart() {
+  document.querySelector("#cart").classList.toggle("active");
+}
+
+// Removing Items from the cart //
+
+function deleteProduct(id1) {
+  let product = products.data.find((item) => {
+    return item.id == id1;
+  });
+  let prod_id = product.id;
+  console.log(prod_id);
+
+  fetch("https://ancient-dawn-92955.herokuapp.com/delete-products", {
+    method: "POST",
+    body: JSON.stringify({
+      id: prod_id,
+    }),
+    headers: {
+      "Content-type": "application/json",
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      if (data["message"] == "Product deleted successfully.") {
+        alert("Deleted succesfully");
+      } else {
+        alert("Products Not Deleted");
+      }
+    });
+
+  console.log(product);
+  console.log(cart);
+}
+
+// Remove Items from Cart //
+function removeItem(id) {
+  let product = products.data.find((item) => {
+    return item.id == id;
+  });
+  //console.log(product);
+
+  cart.splice(
+    cart.findIndex((a) => a.id === product.id),
+    1
+  );
+  renderCart(cart);
 }
